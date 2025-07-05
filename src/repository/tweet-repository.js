a36dfdd4bd53156a5ba9058eee42e71aceba2eb2
simhanik <nikhilsahu0427@gmail.com>
2025-07-05 12:24:58 +0530

    added demo crud for twiiter_demo

---
+++ src/repository/tweet-repository.js
@@ -0,0 +1,55 @@
+const Tweet = require('../model/tweet.js')
+
+class TweetRepository {
+    async create(data){
+        try {
+            const tweet = await Tweet.create(data)
+            return tweet
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+    async get(id){
+        try {
+            const tweet = await Tweet.findById(id)
+            return tweet
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+    async getWithComment(id){
+        try {
+            const tweet = await Tweet.findById(id).populate({path:'comments'})
+            return tweet
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+    
+    async update(tweetId,data){
+        try {
+            const tweet = await Tweet.findByIdAndUpdate(tweetId,data,{new:true})
+            return tweet
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+    async destroy(id){
+        try {
+            const tweet = await Tweet.findByIdAndDelete(id)
+            return tweet
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+}
+
+module.exports = TweetRepository
