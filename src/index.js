fae0855fee973ccfb0d263e48d77098fe81daed6
simhanik <nikhilsahu0427@gmail.com>
2025-07-09 02:25:52 +0530

    added index to the hashtag and removed the hashtag array from tweet

--- src/index.js
+++ src/index.js
@@ -17,16 +17,7 @@ app.listen(PORT, async ()=> {
     console.log(`Server started on Port : ${PORT}`);
     await connect()
     console.log("Mongodb connected");
-    // const tweets = await Tweet.find({
-    //     content:["First tweet","my tweet",'4561649']
-    // })
-    // console.log(tweets);
-    // let response = await tagRepo.findByName(['Trend','Fun'])
-    // console.log(response);
-    // response = response.map((tags) => tags.title)
-    // console.log(response);
-    
-    const tweet = await serRepo.create({content:'is #testing #working ? #excited, it is going to be #Fun'})
+    const tweet = await serRepo.create({content:'captal #CAPITAL #TREND, #fUN #ExciTED'})
     console.log(tweet);
     
 })
